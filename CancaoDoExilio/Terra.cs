f29fa8e9a6386be22b25073ab69e9cf3d01f24e8
Lucas da Silva Nishikawara <lsnishikawara@gmail.com>
2019-01-05 10:02:34 -0200

    o teste como ele veio

---
+++ CancaoDoExilio/Terra.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancaoDoExilio
+{
+    public class Terra
+    {
+        public int QuantidadePalmeirasAvistadas = 7;
+
+        public string sabiaCanta = "Sim";
+
+        protected Palmeira[] palmeiras;
+
+        public List<Ave> aves;
+
+        public bool Terminou
+        {
+            get
+            {
+                return Terminou;
+            }
+            set
+            {
+                Terminou = value;
+            }
+        }
+
+
+        public double ObterQuantidadeCoisas()
+        {
+            double teste = 1.5;
+            for (int i = 0; i < 20; i++)
+            {
+                teste = teste + 1.54;
+            }
+
+            return teste;
+        }
+
+        public bool ObterPrimores()
+        {
+            int x = 20;
+            int y = 10;
+
+            return y == 20 && x == 10;
+        }
+    }
+}
---
+++ CancaoDoExilio/Texto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace CancaoDoExilio
+{
+    public class Texto : ITexto
+    {
+        public int GerarTexto()
+        {
+            return Util.GerarTexto();
+        }
+
+        string ITexto.GerarTexto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
